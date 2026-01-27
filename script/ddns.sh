f22460a2da43cffb354038ddf7f9ef5213c50d16
sunls24 <sunls24@outlook.com>
2026-01-27 10:30:20 +0800

    feat: add shellsb and sing-box config example

--- script/ddns.sh
+++ script/ddns.sh
@@ -1,53 +1,70 @@
 #!/bin/ash
 
-api_token=""
-zone_name="x.xx"
-record_name="x.x.xx"
+set -e
 
-base_dir="/data/ddns"
-cd ${base_dir} || exit
+api_token="!!replace!!"   # cloudflare api token
+zone_name="!!replace!!"   # example.com
+record_name="!!replace!!" # ddns.example.com
 
-# MAYBE CHANGE THESE
-ip=$(curl -s http://ipv4.icanhazip.com)
-ip_file="ip.txt"
-id_file="cloudflare.ids"
+ip4_file="ip4.txt"
+ip6_file="ip6.txt"
 log_file="ddns.log"
+id_file="cloudflare.ids"
 
-# LOGGER
+cd "$(dirname "$0")" || exit
+
 log() {
     if [ "$1" ]; then
-        echo -e "[$(date +'%Y/%m/%d %H:%M:%S')] - $1" >> $log_file
+        echo -e "[$(date +'%Y/%m/%d %H:%M:%S')] - $1" >>"$log_file"
     fi
 }
 
-# SCRIPT START
+cf_zones_id() {
+    curl -s -X GET "https://api.cloudflare.com/client/v4/zones$1" -H "Authorization: Bearer $api_token" | sed -E "s/.+\"result\":\[\{\"id\":\"([a-f0-9]+)\".+/\1/g"
+}
+
+update_ip() {
+    local ip=$1
+    local ip_file=$2
+    local record_type=$3
+    local record_id=$4
+
+    if [ -f "$ip_file" ]; then
+        old_ip=$(cat "$ip_file")
+        if [ "$ip" = "$old_ip" ]; then
+            log "$record_type IP has not changed."
+            return
+        fi
+    fi
+
+    update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$record_id" -H "Authorization: Bearer $api_token" -H "Content-Type: application/json" --data "{\"id\":\"$zone_id\",\"type\":\"$record_type\",\"name\":\"$record_name\",\"content\":\"$ip\"}")
+
+    success=$(echo "$update" | sed -E "s/.+\"success\":[ ]*([a-z]+).+/\1/g")
+    if [ "$success" = "false" ]; then
+        log "API UPDATE FAILED for $record_type. DUMPING RESULTS:\n$update"
+    else
+        echo "$ip" >"$ip_file"
+        log "$record_type IP changed to: $ip"
+    fi
+}
+
 log "Check Initiated"
 
-if [ -f $ip_file ]; then
-    old_ip=$(cat $ip_file)
-    if [ "$ip" = "$old_ip" ]; then
-        log "IP has not changed."
-        exit 0
-    fi
-fi
-
-if [ -f $id_file ] && [ "$(wc -l $id_file | cut -d " " -f 1)" = 2 ]; then
-    zone_id=$(head -1 $id_file)
-    record_id=$(tail -1 $id_file)
+if [ -f "$id_file" ] && [ "$(wc -l "$id_file" | awk '{print $1}')" -ge 3 ]; then
+    zone_id=$(head -1 "$id_file")
+    record_v4=$(head -2 "$id_file" | tail -1)
+    record_v6=$(tail -1 "$id_file")
 else
-    zone_id=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone_name" -H "Authorization: Bearer $api_token" -H "Content-Type: application/json" | sed -E "s/.+\"result\":\[\{\"id\":\"([a-f0-9]+)\".+/\1/g")
-    record_id=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records?name=$record_name" -H "Authorization: Bearer $api_token" -H "Content-Type: application/json" | sed -E "s/.+\{\"id\":\"([a-f0-9]+)\".+\"type\":\"A\".+/\1/g")
-    echo "$zone_id" > $id_file
-    echo "$record_id" >> $id_file
+    zone_id=$(cf_zones_id "?name=$zone_name")
+    record_v4=$(cf_zones_id "/$zone_id/dns_records?name=$record_name&type=A")
+    record_v6=$(cf_zones_id "/$zone_id/dns_records?name=$record_name&type=AAAA")
+    echo "$zone_id" >"$id_file"
+    echo "$record_v4" >>"$id_file"
+    echo "$record_v6" >>"$id_file"
 fi
 
-update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$record_id" -H "Authorization: Bearer $api_token" -H "Content-Type: application/json" --data "{\"id\":\"$zone_id\",\"type\":\"A\",\"name\":\"$record_name\",\"content\":\"$ip\"}")
+ip4=$(curl -s "http://ipv4.icanhazip.com")
+update_ip "$ip4" "$ip4_file" "A" "$record_v4"
 
-success=$(echo "$update" | sed -E "s/.+\"success\":[ ]*([a-z]+).+/\1/g")
-if [ "$success" = "false" ]; then
-    log "API UPDATE FAILED. DUMPING RESULTS:\n$update"
-    exit 1
-else
-    echo "$ip" > $ip_file
-    log "IP changed to: $ip"
-fi
+ip6=$(curl -s "http://ipv6.icanhazip.com")
+update_ip "$ip6" "$ip6_file" "AAAA" "$record_v6"
